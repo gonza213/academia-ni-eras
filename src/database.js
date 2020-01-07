7e21308c7b2147d1f8e1fc9306b854bfdae22ec1
Your Name <you@example.com>
2020-01-07 17:57:21 -0300

    modificar

--- src/database.js
+++ src/database.js
@@ -3,8 +3,7 @@ const mongoose = require('mongoose');
 mongoose.connect('mongodb+srv://root:gonza1991@curso-gysy0.mongodb.net/test?retryWrites=true&w=majority', {
     useCreateIndex: true,
     useFindAndModify: false,
-    useNewUrlParser: true,
-    useUnifiedTopology: true
+    useNewUrlParser: true
 })
 .then(db => console.log('La Base de Datos esta conectado'))
 .catch(err => console.log(err));
