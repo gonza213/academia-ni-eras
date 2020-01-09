fe68394e12d2c5b4016605cc9495bf9e28e83e29
Your Name <you@example.com>
2020-01-09 19:42:45 -0300

    modificacion sitio 1

--- src/routes/lectura.js
+++ src/routes/lectura.js
@@ -2,12 +2,35 @@ const express = require('express');
 const router = express.Router();
 
 const Datos = require('../models/Datos');
+const Image = require('../models/Image');
 const { isAuthenticated } = require('../helpers/auth');
 
 router.get('/lectura/ver-cv', isAuthenticated, async (req, res) => {
-    const datos = await Datos.find().sort({created_at: 'desc'}); //usuarios individuales
+    const datos = await Datos.find().sort({created_at: 'desc'}); 
     res.render('lecturas/ver-cv', {datos});
 });
+
+//ELIMINAR CV
+router.delete('/lectura/ver-cv/:id', isAuthenticated, async (req, res) => {
+    await Datos.findByIdAndDelete(req.params.id);
+    req.flash('success_msg', 'El CV se ha borrado correctamente');
+    res.redirect('/lectura/ver-cv');
+});
+
+
+
+router.get('/lectura/ver-tp', isAuthenticated, async (req, res) => {
+    const trabajos = await Image.find().sort({created_at: 'desc'}); 
+    res.render('lecturas/ver-tp', {trabajos});
+});
+
+//ELIMINAR NOTA
+router.delete('/lectura/ver-tp/:id', isAuthenticated, async (req, res) => {
+    await Image.findByIdAndDelete(req.params.id);
+    req.flash('success_msg', 'El TP se ha borrado correctamente');
+    res.redirect('/lectura/ver-tp');
+});
+
 
 
 module.exports = router;
