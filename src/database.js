aba1a74f61729c06e136d4892d392167d14c8515
Your Name <you@example.com>
2020-01-09 16:11:01 -0300

    mongodb atlas

--- src/database.js
+++ src/database.js
@@ -1,10 +1,5 @@
 const mongoose = require('mongoose');
 
-mongoose.connect('mongodb+srv://root:gonza1991@curso-gysy0.mongodb.net/test', {
-    useCreateIndex: true,
-    useFindAndModify: false,
-    useNewUrlParser: true,
-    useUnifiedTopology: true
-})
+mongoose.connect('mongodb+srv://root:gonza1991@curso-gysy0.mongodb.net/test')
 .then(db => console.log('La Base de Datos esta conectado'))
 .catch(err => console.log(err));
